c1562d5237839ca82f69b95009348f9cb97b1e4b
Marouane Dbibih <marrouane.dbibih@gmail.com>
2025-01-15 10:45:13 +0100

    configue the logs and the jenkins

--- car/src/main/java/org/marouanedbibih/car/modules/car/CarRepository.java
+++ car/src/main/java/org/marouanedbibih/car/modules/car/CarRepository.java
@@ -3,7 +3,10 @@ package org.marouanedbibih.car.modules.car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import io.micrometer.observation.annotation.Observed;
+
 @Repository
+@Observed
 public interface CarRepository extends JpaRepository<Car, Long> {
 
 }
--- client/src/main/java/org/marouanedbibih/client/module/ClientRepository.java
+++ client/src/main/java/org/marouanedbibih/client/module/ClientRepository.java
@@ -3,7 +3,10 @@ package org.marouanedbibih.client.module;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import io.micrometer.observation.annotation.Observed;
+
 @Repository
+@Observed
 public interface ClientRepository extends JpaRepository<Client, Long> {
     
 }
